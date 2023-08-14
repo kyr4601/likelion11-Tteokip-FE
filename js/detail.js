676aafbef59b60362f6157eecd35809b1689040e
ekmonet1 <ekmonet1@naver.com>
2023-08-14 19:22:15 +0900

    LKLN-47 Refactor: 마이페이지 파라미터 할당

--- js/detail.js
+++ js/detail.js
@@ -24,79 +24,26 @@ let infotext3 = document.getElementById('concertPerformer');
 infotext3.innerText = detaildata[0].performer;
 
 
-let seat1 = document.getElementById('Seat1');
-seat1.innerText = detaildata[1].seat1;
-
-let price1 = document.getElementById('Price1');
-price1.innerText = detaildata[1].price1;
-
-let seat2 = document.getElementById('Seat2');
-seat2.innerText = detaildata[1].seat1;
-
-let price2 = document.getElementById('Price2');
-price2.innerText = detaildata[1].price2;
-
-let seat3 = document.getElementById('Seat3');
-seat3.innerText = detaildata[1].seat3;
-
-let price3 = document.getElementById('Price1');
-price3.innerText = detaildata[1].price1;
-
-let seat4 = document.getElementById('Seat4');
-seat4.innerText = detaildata[1].seat4;
-
-let price4 = document.getElementById('Price4');
-price4.innerText = detaildata[1].price4;
-
-let seat5 = document.getElementById('Seat5');
-seat5.innerText = detaildata[1].seat1;
-
-let price5 = document.getElementById('Price5');
-price5.innerText = detaildata[1].price1;
-
-let seat6 = document.getElementById('Seat6');
-seat6.innerText = detaildata[1].seat6;
-
-let price6 = document.getElementById('Price6');
-price6.innerText = detaildata[1].price6;
-
-let seat7 = document.getElementById('Seat7');
-seat7.innerText = detaildata[1].seat7;
-
-let price7 = document.getElementById('Price7');
-price7.innerText = detaildata[1].price7;
-
-
-/*
 
 for (let i = 1; i <= 7; i++) {
+    let seat = document.getElementById(`Seat${i}`);
+    seat.innerText = detaildata[1][`seat${i}`];
 
-    infopriceitem = document.createElement('li');
-
-    var name = document.createElement("span");
-    name.className = 'name' + `${i}`;
-    name.id = "Seat" + `${i}`;
-    name.innerText = detaildata[1].seat`${i}`;
-
-    var price = document.createElement("span");
-    price.className = 'price' + `${i}`;
-    price.id = "Price" + `${i}`;
-    price.innerText = detaildata[1].price`${i}`;
-
-    infopriceitem.appendChild(name, price);
-    infopricelist.appendChild(infopriceitem);
-
+    let price = document.getElementById(`Price${i}`);
+    price.innerText = detaildata[1][`price${i}`];
 }
-
-
-*/
 
 
 /*하이퍼링크*/
 
-function moveRaffle() {
-    location.href = "../html/raffle.html";
-}
+
+const goraffle = document.getElementById('goRaffle');
+
+goraffle.addEventListener("click", function (event){
+    applybtn = searchResult.innerText;
+    window.location.href = 'raffle.html?raffle=' + encodeURIComponent(applybtn);
+})
+
 
 function moveMypage() {
     location.href = "../html/mypage.html";
@@ -108,8 +55,8 @@ function moveMain() {
 
 /*calendar*/
 
-var today = new Date();//오늘 날짜//내 컴퓨터 로컬을 기준으로 today에 Date 객체를 넣어줌
-var date = new Date();//today의 Date를 세어주는 역할
+let today = new Date();//오늘 날짜//내 컴퓨터 로컬을 기준으로 today에 Date 객체를 넣어줌
+let date = new Date();//today의 Date를 세어주는 역할
 
 window.onload = function(){
     buildCalendar();
@@ -129,12 +76,12 @@ function nextCalendar() {
 /*현재 달 달력 만들기*/
 
 function buildCalendar(){
-    var doMonth = new Date(today.getFullYear(),today.getMonth(),1);
+    let doMonth = new Date(today.getFullYear(),today.getMonth(),1);
     //이번 달의 첫째 날,
-    var lastDate = new Date(today.getFullYear(),today.getMonth()+1,0);
+    let lastDate = new Date(today.getFullYear(),today.getMonth()+1,0);
     //이번 달의 마지막 날
-    var tbCalendar = document.getElementById("calendar");
-    var tbCalendarYM = document.getElementById("tbCalendarYM");
+    let tbCalendar = document.getElementById("calendar");
+    let tbCalendarYM = document.getElementById("tbCalendarYM");
     tbCalendarYM.innerHTML = today.getFullYear() + ". " + (today.getMonth() + 1) ;
 
     while (tbCalendar.rows.length > 2) {
@@ -142,14 +89,15 @@ function buildCalendar(){
         //기본 열 크기는 body 부분에서 2로 고정되어 있다.
         tbCalendar.deleteRow(tbCalendar.rows.length-1);
     }
+
     let row = null;
     row = tbCalendar.insertRow();
     row.className = "dateList";
     //테이블에 새로운 열 삽입
-    var cnt = 0;// count, 셀의 갯수를 세어주는 역할
+    let cnt = 0;// count, 셀의 갯수를 세어주는 역할
     // 1일이 시작되는 칸을 맞추어 줌
 
-    var anyDate = 27;
+    let anyDate = 27;
 
     for (i=0; i<doMonth.getDay(); i++) {
         /*이번달의 day만큼 돌림*/
@@ -176,9 +124,13 @@ function buildCalendar(){
             row = calendar.insertRow();
         }
         /*지정된 날짜에 색 칠하기*/
+
         if (today.getFullYear() == date.getFullYear()
             && today.getMonth() == date.getMonth()
             && i == anyDate) {
+
+
+            console.log(today.getMonth())
             cell.bgColor = "#26ddb1";//셀의 배경색
             cell.style.border = "1px solid #26ddb1";
             cell.style.borderRadius = "60px" ;
@@ -186,6 +138,7 @@ function buildCalendar(){
         }
     }
 }
+
 /*
 
 
