5b9ab7c7eda85290cac712e840ec0eb2d8ae4055
kyr4601 <jane4545@naver.com>
2023-08-18 00:59:09 +0900

    LKLN-54 feat: api 연결작업

--- js/raffle.js
+++ js/raffle.js
@@ -1,11 +1,58 @@
-/*남은 작업
-1. detail에서 url로 넘긴 콘서트 제목 받아오기
-2. 해당 제목을 get의 params로 담아 해당 콘서트의 정보(장소, 일시, 구역가격, 사진) data 받아오기
-3. title-place, time, area-price, img에 data 채우기
-4. 결제하기 버튼 -> post - item name(콘서트 제목), 구역, 티켓가격, 매수
- */
 
-const baseUrl = "http://ec2-3-38-100-226.ap-northeast-2.compute.amazonaws.com:8080";
+const baseUrl = "http://ec2-3-34-90-9.ap-northeast-2.compute.amazonaws.com:8080";
+
+const getiteminfo = () => {
+    axios.get(baseUrl + '/api/items/search',{
+        params: {
+            itemName: getTitleFromURL()
+        }
+    }).then(response =>{
+        console.log(response.data);
+        let itemdata = response.data;
+
+        document.getElementById('venue').innerText = itemdata.venue;
+        document.getElementById('dateTime').innerText = itemdata.dateTime;
+        document.getElementById('postimg').src=itemdata.post;
+
+        let itemid = itemdata.id;
+
+        getseatinfo(itemid);
+
+
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
+
+const getseatinfo = (id) => {
+    axios.get(baseUrl + '/api/sections/item',{
+        params: {
+            itemId: id
+        }
+    }).then(response =>{
+        console.log(response.data);
+        let seatdata = response.data;
+
+
+        for (let i = 0; i <= 6; i++) {
+            let seat = document.getElementById(`seat${i}`);
+            seat.innerText = seatdata[i].sectionName;
+
+            let price = document.getElementById(`price${i}`);
+            price.innerText = seatdata[i].price+ ' 원';
+
+            let seatQuantity = document.getElementById(`seatQuantity${i}`);
+            seatQuantity.innerText = seatdata[i].seatQuantity+ '석';
+
+
+        }
+
+
+
+    }).catch(function (error) {
+        console.log(error);
+    })
+}
 
 const paybox = document.getElementById("pay");
 const areaElements = document.querySelectorAll('.area-a, .area-b, .area-c, .area-d, .area-e, .area-f, .area-g');
@@ -51,29 +98,34 @@ for (const area of areaElements) {
 }
 
 const paybtn = document.getElementById("payBtn");
+paybtn.addEventListener('click', () => {
+    createRaffle();
+});
 
-paybtn.addEventListener("click", function (){
-    //외않되는대
-    /*
-    if(paybox.style.display === "none"){
-        alert('구역을 선택해주세요.')
-    }else{
-        alert('응모가 완료되었습니다.' + '\n' + 'Koun이 당신의 행운을 빕니다.');
-        window.location.href = "main.html";
-    }*/
+
+const createRaffle = () => {
+    let sectionid;
+    for(i = 0;i<7;i++){
+        let seat = document.getElementById(`seat${i}`);
+        let chosen = document.getElementById("selectArea");
+        if(seat.innerText == chosen.innerText){
+            sectionid = i;
+            console.log(sectionid+1);
+        }
+    }
     axios.post(baseUrl + "/api/raffles", {
         "raffleCount": quantity.innerText,
-        "raffleDrawDate": "2023-08-04 15:30",
+        "raffleDrawDate": getFormattedDateTime(),
         "userId": 1,
-        "itemId": 2,
-        "sectionId": 2
+        "itemName": getTitleFromURL(),
+        "sectionId": sectionid+1
     }).then(function (response) {
-        console.log(quantity.innerText);
         alert('응모가 완료되었습니다.' + '\n' + 'Koun이 당신의 행운을 빕니다.');
+        window.location.href = "main.html";
     }).catch(function (error) {
         console.log("error")
     })
-})
+}
 
 
 /*콘서트 제목으로 detail-raffle 연결*/
@@ -85,10 +137,25 @@ function getTitleFromURL() {
 function renderResult() {
     const titleTerm = getTitleFromURL();
     const Result = document.getElementById('mainTitle');
-    // 서버에서 검색 결과를 가져오는 로직을 구현하고 해당 결과를 페이지2에 렌더링하는 작업을 수행한다.
     Result.innerText = titleTerm;
 }
 
-// 페이지가 로드되었을 때 검색 결과를 렌더링
-window.addEventListener('DOMContentLoaded', renderResult);
+function getFormattedDateTime() {
+    const currentDate = new Date();
+
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+
+    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}`;
+
+    return formattedDateTime;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    renderResult();
+    getiteminfo();
+});
 
